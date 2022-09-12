d2fd727f049b1f54cf96f289a0e08b425ae11ee3
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-09-12 17:59:02 +0200

    First commit

---
+++ VisitMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VisitMe
+//
+//  Created by Amjad Oudeh on 12.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VisitMe/VisitMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisitMeApp.swift
+//  VisitMe
+//
+//  Created by Amjad Oudeh on 12.09.22.
+//
+
+import SwiftUI
+
+@main
+struct VisitMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
