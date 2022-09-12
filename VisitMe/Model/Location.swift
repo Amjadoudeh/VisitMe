984903c98aa8cae4f0df0fe9c1c1acb99d170e5d
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-09-12 18:32:27 +0200

    Create a Model

---
+++ VisitMe/Model/Location.swift
@@ -0,0 +1,11 @@
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
